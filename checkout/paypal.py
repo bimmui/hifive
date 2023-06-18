b1ee69ec0dabbbdd6c1c3acac5e7ba5162478409
Daniel Opara <58013370+bimmui@users.noreply.github.com>
2023-06-18 10:30:04 -0400

    Replace client ID and secret key with filler and provided documentation on how to get them
    NO YOU CANNOT USE THE KEYS FROM PREVIOUS COMMITS, I DELETED THEM

--- checkout/paypal.py
+++ checkout/paypal.py
@@ -5,8 +5,9 @@ from paypalcheckoutsdk.core import PayPalHttpClient, SandboxEnvironment
 
 class PayPalClient:
     def __init__(self):
-        self.client_id = "AUdym9sqk-OJr0aFbmAyOmzOcoDyrJQdDeB0Dm9SYwiLOOoJEBmERhN4q11yGQGeBEZU0oNRkruA4W0F"
-        self.client_secret = "ENL_VHg9HeM-GOUU7onbYiPDaXqDyhkcxNQ33rR3563JlW2J7sIF34JavbPPfdCRoA5MtjJRUMgrPrGe"
+        # https://www.upwork.com/resources/paypal-client-id-secret-key
+        self.client_id = "CLIENT ID"
+        self.client_secret = "SECRET KEY"
         self.environment = SandboxEnvironment(client_id=self.client_id, client_secret=self.client_secret)
         self.client = PayPalHttpClient(self.environment)
         print(self.environment, '\n', self.client)
